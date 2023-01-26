b2c3f58a7cc0f441809ca7c822870f1e96fb7534
Jakub Šváb <69170498+kubikula59@users.noreply.github.com>
2023-01-26 20:01:19 +0100

    feat: added request to python model
    result is now returned to main page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import * as url from "url";
 import {scrapePage} from "./scrapePage.cjs";
 import axios from 'axios';
+import fetch from 'node-fetch';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
@@ -21,14 +22,21 @@ app.post("/api/process", (req, res) => {
 
   if(!isValidHttpUrl(req.body.searchUrl))
   {
-    res.send({msg: "Chyba! Nesprávná url"})
+    res.send({msg: "Wrong! Incorrect url. Check it and try again"})
     return;
   }
   let scrapeStuff = scrapePage(req.body.searchUrl);
 
-  scrapeStuff.then(function (result) {
-    console.log(result);
-    res.send({msg: result})
+  scrapeStuff.then(async function (result) {
+
+    // Make request to our python API
+    const response = await fetch('http://127.0.0.1:5000/classify', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "text": result[0] })
+        });
+        const apiresponse = await response.json();
+        res.send({msg: apiresponse.prediction})
   });
 
 
